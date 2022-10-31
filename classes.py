8a936e5334e217252a77e1b2f4e544ca148c33fa
YourFBIAgent <nedzaddonlagic04@gmail.com>
2022-10-31 22:10:20 +0100

    Changed Player class to not inherit sprite class
    Also added white line to display on the top with
    the circle not being able to go past it.

--- classes.py
+++ classes.py
@@ -1,5 +1,6 @@
 import pygame
 
+from functions import getPercent
 class Clock:
     def __init__(self, fps):
         self.clock = pygame.time.Clock()
@@ -8,26 +9,31 @@ class Clock:
     def tick(self):
         self.clock.tick(self.FPS)
 
-class Player(pygame.sprite.Sprite):
+class Player:
     def __init__(self, circle_radius, color, pos, width, height):
-        super().__init__()
         self.COLOR = color
         self.pos = pos 
         self.WIDTH = width
         self.HEIGHT = height
         self.SPEED = 10
 
-        self.updateCircle(circle_radius)
+        self.createCircle(circle_radius)
+        self.createLine(width, height, 5, 10)
 
-    def updateCircle(self, circle_radius):
+    def createCircle(self, circle_radius):
         ball_img = pygame.Surface((2 * circle_radius, 2 * circle_radius), pygame.SRCALPHA)
         pygame.draw.circle(ball_img, self.COLOR, (circle_radius, circle_radius) , circle_radius)
         self.image = ball_img
         self.rect = self.image.get_rect( center = self.pos )
 
+    def createLine(self, width, height, line_height, screen_percent):
+        line_img = pygame.image.load('./img/line.png').convert()
+        self.line_img = pygame.transform.scale(line_img, (width, line_height))
+        self.line_rect = line_img.get_rect( topleft = (0, getPercent(height, screen_percent)) )
+
     def update(self):
         keys = pygame.key.get_pressed()
-        if keys[pygame.K_w] and self.rect.top - self.SPEED >= 0:
+        if keys[pygame.K_w] and self.rect.top >= self.line_rect.bottom:
             self.rect.top -= self.SPEED
         elif keys[pygame.K_s] and self.rect.bottom + self.SPEED <= self.HEIGHT:
             self.rect.bottom += self.SPEED
@@ -35,3 +41,8 @@ class Player(pygame.sprite.Sprite):
             self.rect.right += self.SPEED
         elif keys[pygame.K_a] and self.rect.left - self.SPEED >= 0:
             self.rect.left -= self.SPEED
+
+    def draw(self, screen):
+        screen.blit(self.image, self.rect)
+        screen.blit(self.line_img, self.line_rect)
+        pygame.display.flip()
