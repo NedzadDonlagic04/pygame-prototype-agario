dfd51579e742c6652ad91cc2288c24c05482bcea
YourFBIAgent <nedzaddonlagic04@gmail.com>
2022-11-01 08:30:02 +0100

    Added function to get distance between 2 points

--- functions.py
+++ functions.py
@@ -1,6 +1,13 @@
+from math import sqrt, pow
+
 def getPercent(total, percent):
     if percent < 1 or percent > 100:
         percent = 50
 
     percent = total * percent / 100
     return round(percent)
+
+def getDistance(offSetX1, offSetY1, offSetX2, offSetY2):
+    a = offSetX2 - offSetX1
+    b = offSetY2 - offSetY1
+    return sqrt(pow(a, 2) + pow(b, 2))
